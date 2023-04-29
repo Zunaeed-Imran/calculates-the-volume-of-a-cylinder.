afec4a22b8cae298d0defe0edf4e06cd2c4bcfab
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-29 19:26:24 +0600

    Volume of Cylinder in javaScript

---
+++ cylinder.js
@@ -0,0 +1,6 @@
+let height = prompt("Enter the Height of Cylinder: ");
+let radius = prompt("Enter the radius of Cylinder: ");
+
+let result = Math.PI * Math.pow(radius, 2) * height;
+
+document.write("The area of Cylinder is " + result);
